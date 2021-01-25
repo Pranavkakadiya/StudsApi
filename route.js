9f2e00fe7de19be381d6f3101ebd5757a7abfa66
Pranav <pkakadiya274@rku.ac.in>
2021-01-25 22:46:45 +0530

    create a new model

--- route.js
+++ route.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const Product=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
-// const stud=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
+const Book=require('./Model/product')//model no use kari ne ahiya thi data post k get karsu
 
 const router=express.Router();//router no use kari ne kevanu k avo data fetch karo avo data post karo
 
@@ -15,7 +15,7 @@ router.get("/products",async (req,res)=>{   //aa path game te chale singular plu
 
 router.get("/books",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
 
-    const book1=await Product.find();//u should await for result result ave chhe etle Products are name of collections
+    const book1=await Book.find();//u should await for result result ave chhe etle Products are name of collections
     res.send(book1);//save respons in book
 })
 
