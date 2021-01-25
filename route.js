c7cf256c0d2527050703d7c5f0adb7f128623c8a
Pranav <pkakadiya274@rku.ac.in>
2021-01-25 21:57:35 +0530

    comment studs

--- route.js
+++ route.js
@@ -1,22 +1,22 @@
 //use router class which in express library
 const express = require('express')
 
-const Products=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
-const stud=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
+const Product=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
+// const stud=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
 
 const router=express.Router();//router no use kari ne kevanu k avo data fetch karo avo data post karo
 
 
 router.get("/products",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
 
-    const book=await Products.find();//u should await for result result ave chhe etle Products are name of collections
+    const book=await Product.find();//u should await for result result ave chhe etle Products are name of collections
     res.send(book);//save respons in book
 })
 
-router.get("/studs",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
+// router.get("/studs",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
 
-    const book=await stud.find();//u should await for result result ave chhe etle Products are name of collections
-    res.send(book);//save respons in book
-})
+//     const book=await stud.find();//u should await for result result ave chhe etle Products are name of collections
+//     res.send(book);//save respons in book
+// })
 
 module.exports =router;
