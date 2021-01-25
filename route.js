038ac487667f171a1039060a87ef3bdf4ea2c913
Pranav <pkakadiya274@rku.ac.in>
2021-01-25 22:03:48 +0530

    books ass

--- route.js
+++ route.js
@@ -13,10 +13,10 @@ router.get("/products",async (req,res)=>{   //aa path game te chale singular plu
     res.send(book);//save respons in book
 })
 
-// router.get("/studs",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
+router.get("/books",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
 
-//     const book=await stud.find();//u should await for result result ave chhe etle Products are name of collections
-//     res.send(book);//save respons in book
-// })
+    const book1=await Product.find();//u should await for result result ave chhe etle Products are name of collections
+    res.send(book1);//save respons in book
+})
 
 module.exports =router;
