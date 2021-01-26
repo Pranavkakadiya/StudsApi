87347197a837655e7d75f07d5584c0e5f20985da
Pranav <pkakadiya274@rku.ac.in>
2021-01-26 10:44:44 +0530

    change in student route

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const Product=require('./Model/book')//model no use kari ne ahiya thi data post k get karsu
 const Book=require('./Model/product')//model no use kari ne ahiya thi data post k get karsu
-const stud=require('./Model/stud')//model no use kari ne ahiya thi data post k get karsu
+const Student=require('./Model/stud')//model no use kari ne ahiya thi data post k get karsu
 
 const router=express.Router();//router no use kari ne kevanu k avo data fetch karo avo data post karo
 
@@ -21,7 +21,7 @@ router.get("/books",async (req,res)=>{   //aa path game te chale singular plural
 })
 router.get("/studs",async (req,res)=>{   //aa path game te chale singular plural or other  type is await becoz nodejs script run on asynchronous manner
 
-    const book2=await stud.find();//u should await for result result ave chhe etle Products are name of collections
+    const book2=await Student.find();//u should await for result result ave chhe etle Products are name of collections
     res.send(book2);//save respons in book
 })
 module.exports =router;
