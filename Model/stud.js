4339a8e12205947d43d08b186adb5ef6e1c309b8
Pranav <pkakadiya274@rku.ac.in>
2021-01-29 16:23:19 +0530

    change again in schema 5

--- Model/stud.js
+++ Model/stud.js
@@ -3,15 +3,15 @@
 const mongoose = require('mongoose');
 //schema define
 const schema = mongoose.Schema({
-    s_id: {type:String},
+    //s_id: {type:String},
     name: {type:String},
-    email: {type:String},
-    address: [{ city: {type:String}, state: {type:String} }],
-    marks: [{ science: {type:Number}, chemistry: {type:Number}, maths: {type:Number} }],
-    status: {type:Boolean},
-    play: [String],
-    birthday: {type:Date},
-    media: [{ youtube: {type:Boolean}, instagram: {type:Boolean}, snapchat: {type:Boolean} }]
+    email: {type:String}
+    // address: [{ city: {type:String}, state: {type:String} }],
+    // marks: [{ science: {type:Number}, chemistry: {type:Number}, maths: {type:Number} }],
+    // status: {type:Boolean},
+    // play: [String],
+    // birthday: {type:Date},
+    // media: [{ youtube: {type:Boolean}, instagram: {type:Boolean}, snapchat: {type:Boolean} }]
 })
 
 
--- route.js
+++ route.js
@@ -32,15 +32,15 @@ router.post('/studs', async (req, res) => {
     const stud = new Stud({
         // name:req.body.name,
         // qty:req.body.qty,//jo ahiya 3rd filed pass karu to thay k na thay
-        s_id: req.body.s_id,
+        //s_id: req.body.s_id,
         name: req.body.name,
-        email: req.body.email,
-        address: req.body.address,
-        marks: req.body.marks,
-        status: req.body.status,
-        play: req.body.play,
-        birthday: req.body.birthday,//birthday:newDate('Jan2,2001')
-        media: req.body.media
+        email: req.body.email
+        // address: req.body.address,
+        // marks: req.body.marks,
+        // status: req.body.status,
+        // play: req.body.play,
+        // birthday: req.body.birthday,//birthday:newDate('Jan2,2001')
+        // media: req.body.media
     });
 
     await stud.save();//object enter thase db ma backend ma insertOne vali query fire thase 
