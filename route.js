958b690507a59b91c86c4120eec377a2086beaf6
Pranav <pkakadiya274@rku.ac.in>
2021-02-01 15:42:57 +0530

    studs changes

--- route.js
+++ route.js
@@ -37,15 +37,15 @@ router.post('/studs', async (req, res) => {
 
     try {
         const stud = new Stud({
-            // s_id: req.body.s_id,
+            s_id: req.body.s_id,
             name: req.body.name,
-            email: req.body.email
-            // address: req.body.address,
-            // marks: req.body.marks,
-            // status: req.body.status,
-            // play: req.body.play,
-            // birthday: req.body.birthday,
-            // media: req.body.media
+            email: req.body.email,
+            address: req.body.address,
+            marks: req.body.marks,
+            status: req.body.status,
+            play: req.body.play,
+            birthday: req.body.birthday,
+            media: req.body.media
         });
     
         await stud.save();
